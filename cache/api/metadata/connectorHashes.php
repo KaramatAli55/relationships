471e1e714a9b966da7e1b967e86133119ef8d8f4
Karamat Ali <karamat.ali@RT-LT-254.rolustech.net>
2017-09-27 16:51:22 +0500

    fullsugar

---
+++ cache/api/metadata/connectorHashes.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2017-09-27 11:50:33
+$hashes = array (
+  'connectors' => 'c8d8d29eeca7465654442c12bab33645',
+);
---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2017-09-27 11:48:06
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Prospects' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Opportunities' => 
+  array (
+  ),
+);
